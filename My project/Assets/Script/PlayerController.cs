aca235581c97456078008d29a3e37566f0841a70
O-S-H <dhtjdgus153@naver.com>
2022-03-21 00:46:40 +0900

    save
    save

--- My project/Assets/Script/PlayerController.cs
+++ My project/Assets/Script/PlayerController.cs
@@ -5,44 +5,85 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
-    Transform trnas;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        spriteRenderer= GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-    private void Update() //1초에 60프레임
+    void Update() //1초에 60프레임
     {
-        if (Input.GetButtonUp ("Horizontal"))
+        if (Input.GetButtonUp("Horizontal"))
         {
-            rigid.velocity= new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
         //캐릭터 방향 전환
         if (Input.GetButtonDown("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-            //trnas.localScale.x =Input.GetAxisRaw("Horizontal") == -1;  //왜 안되냐 ..
+
+
         }
-           
+        if (Mathf.Abs(rigid.velocity.x) < 0.5)
+        {
+            anim.SetBool("isWalk", false);
+
+        }
+        else
+        {
+            anim.SetBool("isWalk", true);
+        }
+
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJump", true);
+
+
+        }
+
 
 
 
     }
 
-   
+
     void FixedUpdate()  // 1초에 50프레임
     {
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
         if (rigid.velocity.x >= maxSpeed)
+        {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); //Right
-        else if (rigid.velocity.x <maxSpeed*(-1))
-            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);//left
+        }
+          
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);//left
+        }
+
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Paltform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 1)
+                {
+                    anim.SetBool("isJump", false);
+                }
+
+            }
+        }
+        
     }
-}
+} 
